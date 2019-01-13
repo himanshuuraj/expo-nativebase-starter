7045366268a28281af25d761b3b0e31f156c58b8
himanshu <himanshu.raj@byg.co.in>
2019-01-13 22:05:06 +0530

    login done

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./components/login";
+import FontAwesome from "./node_modules/@expo/vector-icons/fonts/FontAwesome.ttf";
+import MaterialIcons from "./node_modules/@expo/vector-icons/fonts/MaterialIcons.ttf";
+
+import { Font, AppLoading } from "expo";
+import Expo from "expo";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    console.log("Hello");
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) return null;
     return (
       <View style={styles.container}>
         <Login />
--- components/login.js
+++ components/login.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { Font, AppLoading } from "expo";
 import {
   Container,
   Header,
   Content,
-  Card,
-  CardItem,
-  Body,
-  Text
+  Text,
+  Button,
+  Form,
+  Item,
+  Label,
+  Input
 } from "native-base";
+
+import { View } from "react-native";
 
 export default () => {
   return (
@@ -17,14 +22,53 @@ export default () => {
       }}
     >
       <Header />
-      <Content>
-        <Card>
-          <CardItem>
-            <Body>
-              <Text>//Your text here</Text>
-            </Body>
-          </CardItem>
-        </Card>
+      <Content
+        contentContainerStyle={{
+          padding: 0
+        }}
+      >
+        <Container
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%"
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 25,
+              fontWeight: "bold"
+            }}
+          >
+            Bhaiyari
+          </Text>
+          <Form
+            style={{
+              width: "90%",
+              backgroundColor: "light grey"
+            }}
+          >
+            <Item floatingLabel>
+              <Label>Email or Phone number</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+          <View
+            style={{
+              marginTop: 10,
+              width: "90%"
+            }}
+          >
+            <Button disabled block>
+              <Text>login</Text>
+            </Button>
+          </View>
+        </Container>
       </Content>
     </Container>
   );
